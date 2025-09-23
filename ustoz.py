a37be5591453ee707469b3a56989231d97021faf
Xuzayfa <maripaliyevxuzayfa1@gmail.com>
2025-09-23 13:06:24 +0500

    asdjflk

--- ustoz.py
+++ ustoz.py
@@ -24,6 +24,21 @@
 
 # iteratorlar
 
+# string, intager, boolen, float, list, dict, set, tuple
+
+# int, float, boolen | not iterable
+
+# string, list, dict, set, tuple | iterable
+
+# for s in ("salom", "fasd"):
+#     print(s)
 
 
+mevalar = ["olma", "behi", "anor"]
+mevalar_iter = iter(mevalar)
+print(next(mevalar_iter))
+print("salom")
+print(next(mevalar_iter))
+print(next(mevalar_iter))
+# print(next(mevalar_iter))
 
