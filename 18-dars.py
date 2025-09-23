9f99c84bb50ca23897500508f5139ac73230a8ac
ziyodullo <ziyodullodev@gmail.com>
2025-09-23 13:43:40 +0500

    added:something

--- 18-dars.py
+++ 18-dars.py
@@ -1,5 +1,5 @@
 
-print('saloooommmmm!!!!')
+# print('saloooommmmm!!!!')
 
 
 '''
@@ -19,18 +19,56 @@ git clone + ссылка самого проекта (https://github.com/mixiii0
 # decorators
 
 
-def dekorator(func):
-    def luboy():
-        print("dekorator birinchi")
-        func()
-        print("dekorator ikkinchi")
+# def dekorator(func):
+#     def luboy():
+#         print("dekorator birinchi")
+#         func()
+#         print("dekorator ikkinchi")
 
-    return luboy
+#     return luboy
 
-@dekorator
-def salomlash():
-    print("Assalomu alaykum")
+# @dekorator
+# def salomlash():
+#     print("Assalomu alaykum")
 
 
-salomlash()
+# salomlash()
+
+
+
+# def dekorator(nom):
+#     def nik():
+#         print('Salooommm 1111')
+#         nom()
+#         print('Byeeeeeee 222')
+    
+#     return nik
+# @dekorator
+# def salomlash():
+#     print('assalomu aleykum')
+
+# salomlash()
+
+
+
+ism = ['Mukhammad']
+iteration = iter(ism)
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+
+
+
+
+
+
+
+
+
 
