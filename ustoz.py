7702f1866e6398bc07788bf9e138857c5ba472df
Xuzayfa <maripaliyevxuzayfa1@gmail.com>
2025-09-23 12:46:03 +0500

    asdjflk

---
+++ ustoz.py
@@ -0,0 +1,21 @@
+
+
+
+
+# decorators
+
+
+def dekorator(func):
+    def luboy():
+        print("dekorator birinchi")
+        func()
+        print("dekorator ikkinchi")
+
+    return luboy
+
+@dekorator
+def salomlash():
+    print("Assalomu alaykum")
+
+
+salomlash()
