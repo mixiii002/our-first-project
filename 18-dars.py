581657b4b336087c638f67c89f8258e8bbec8581
ziyodullo <ziyodullodev@gmail.com>
2025-09-23 12:23:14 +0500

    yengili

--- 18-dars.py
+++ 18-dars.py
@@ -2,3 +2,17 @@
 print('saloooommmmm!!!!')
 
 
+'''
+для того чтобы поделиться изменением:
+git add . 
+git commit -m "что-то"
+git push
+
+для того чтобы получить изменения с других:
+git pull
+
+для того чтобы клонировать актуальный проект:
+git clone + ссылка самого проекта (https://github.com/mixiii002/our-first-project.git)
+'''
+
+
